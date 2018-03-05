7ab7c7b517ceb5b4c07fa41f28a295c7e67de528
Tadej Stanic <stanic.tadej@gmail.com>
2018-03-05 13:06:17 +0100

    update webpack and its dependencies to version 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const { PATHS, HOST, PORT, THEME_NAME } = require('./env.config');
-const utils = require('./scripts/utils');
-const webpack = require('webpack');
-const path = require('path');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { PATHS, HOST, PORT, THEME_NAME } = require("./env.config");
+const utils = require("./scripts/utils");
+const webpack = require("webpack");
+const path = require("path");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const ENV = utils.getEnv();
 const WATCH = global.watch || false;
@@ -14,30 +14,34 @@ module.exports = {
 
   output: {
     path: PATHS.compiled(),
-    publicPath: ENV === 'production' ? '/' : `http://${HOST}:${PORT}/wp-content/themes/${THEME_NAME}/`,
-    filename: 'js/[name].js',
-    sourceMapFilename: '[file].map',
+    publicPath:
+      ENV === "production"
+        ? "/"
+        : `http://${HOST}:${PORT}/wp-content/themes/${THEME_NAME}/`,
+    filename: "js/[name].js",
+    sourceMapFilename: "[file].map"
   },
 
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         include: PATHS.src()
       },
       {
         test: /\.css$/,
         loader: getCssLoader()
-      },
+      }
     ]
   },
 
-  devtool: ENV === 'production' ? false : 'inline-source-map',
+  devtool: ENV === "production" ? false : "inline-source-map",
 
   plugins: getPlugins(ENV),
 
-  target: 'web',
+  target: "web",
 
   watch: WATCH
 };
@@ -48,30 +52,32 @@ module.exports = {
 
 function getEntry() {
   const entry = {};
-  entry.main = [PATHS.src('js', 'main.js')];
-  if (ENV === 'development') entry.main.push('webpack-hot-middleware/client');
+  entry.main = [PATHS.src("js", "main.js")];
+  if (ENV === "development") entry.main.push("webpack-hot-middleware/client");
   return entry;
 }
 
-
 function getPlugins(env) {
   const plugins = [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(env) }),
-    new CopyWebpackPlugin([{ from: PATHS.src('assets'), to: 'assets' }], { ignore: ['*.psd'] })
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify(env) }),
+    new CopyWebpackPlugin([{ from: PATHS.src("assets"), to: "assets" }], {
+      ignore: ["*.psd"]
+    })
   ];
 
   switch (env) {
-
-    case 'production':
-      plugins.push(new ExtractTextPlugin('main.css'));
-      plugins.push(new webpack.optimize.UglifyJsPlugin({
-        sourceMap: true,
-        minimize: true,
-        compress: { warnings: false }
-      }));
+    case "production":
+      plugins.push(new ExtractTextPlugin("main.css"));
+      // plugins.push(
+      //   new webpack.optimize.UglifyJsPlugin({
+      //     sourceMap: true,
+      //     minimize: true,
+      //     compress: { warnings: false }
+      //   })
+      // );
       break;
 
-    case 'development':
+    case "development":
       plugins.push(new webpack.HotModuleReplacementPlugin());
       plugins.push(new webpack.NoEmitOnErrorsPlugin());
       plugins.push(new WriteFilePlugin());
@@ -82,12 +88,12 @@ function getPlugins(env) {
 }
 
 function getCssLoader() {
-  if (ENV === 'production') {
+  if (ENV === "production") {
     return ExtractTextPlugin.extract({
-      fallbackLoader: "style-loader",
-      loader: "css-loader?importLoaders=1&minimize=true!postcss-loader"
+      fallback: "style-loader",
+      use: "css-loader?importLoaders=1&minimize=true!postcss-loader"
     });
   } else {
-    return 'style-loader!css-loader?importLoaders=1!postcss-loader';
+    return "style-loader!css-loader?importLoaders=1!postcss-loader";
   }
 }
